9ad26d2211fe1c13d158cdcb6e0a31f35197cc50
Emirhan Dogandemir <emirhandgndmr51@gmail.com>
2021-07-08 13:31:07 +0300

    utilities in core layer(Result finished)

---
+++ src/main/java/com/koc/hospital/core/utilities/results/ErrorResult.java
@@ -0,0 +1,13 @@
+package com.koc.hospital.core.utilities.results;
+
+public class ErrorResult extends Result{
+
+	public ErrorResult(boolean success) {
+		super(false);
+	}
+	
+	public ErrorResult(String message) {
+		super(false,message);
+	}
+
+}
---
+++ src/main/java/com/koc/hospital/core/utilities/results/SuccessResult.java
@@ -0,0 +1,14 @@
+package com.koc.hospital.core.utilities.results;
+
+public class SuccessResult extends Result{
+
+	public SuccessResult(boolean success) {
+		super(true);
+		
+	}
+	public SuccessResult(String message) {
+		super(true,message);
+		
+	}
+
+}
